cf2479f51cc47668e59a12ef20a2be447ed37fc3
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-07 21:31:49 +0100

    empty project

---
+++ Editor/YourDefaultEditorScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace PabloSorribes.FmodHelper
+{
+	public class YourDefaultEditorScript : Editor 
+	{
+		//TestyTest    
+	}
+}
