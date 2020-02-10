46428a72228228ecc1b813cae45ad34d328743d2
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-11 00:40:07 +0100

    Revert "added audio manager and removed asmdef files"
    This reverts commit 926cf0e8451e4460e75a2f8256de005630cbec82.
    
    + adds scripts

---
+++ Runtime/FmodHelperExamples.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PabloSorribes.FmodHelper
+{
+	public class FmodHelperExamples : MonoBehaviour
+	{
+		private void Start()
+		{
+			PabloSorribes.FmodHelper.AudioManager.Instance.PlayOneShot("event:/TestEvent");
+
+		}
+
+	}
+}
--- Runtime/GenericSingleton.cs
+++ Runtime/GenericSingleton.cs
@@ -1,82 +1,86 @@
 ﻿using UnityEngine;
 
-public abstract class GenericSingleton<T> : Singleton where T : MonoBehaviour
+namespace PabloSorribes.FmodHelper
 {
-	private static T _instance;
-	private static object _lock = new object();
 
-	public static T Instance
+	public abstract class GenericSingleton<T> : Singleton where T : MonoBehaviour
 	{
-		get
-		{
-			if (ApplicationIsQuitting)
-			{
-				Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
-					"' will not be returned because the application is quitting.");
-				return null;
-			}
+		private static T _instance;
+		private static object _lock = new object();
 
-			lock (_lock)
+		public static T Instance
+		{
+			get
 			{
-				if (_instance != null)
+				if (ApplicationIsQuitting)
 				{
-					return _instance;
+					Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+						"' will not be returned because the application is quitting.");
+					return null;
 				}
 
-				var instances = FindObjectsOfType<T>();
-
-				if (instances.Length > 0)
+				lock (_lock)
 				{
-					if (instances.Length == 1)
+					if (_instance != null)
 					{
+						return _instance;
+					}
+
+					var instances = FindObjectsOfType<T>();
+
+					if (instances.Length > 0)
+					{
+						if (instances.Length == 1)
+						{
+							return _instance = instances[0];
+						}
+
+						Debug.LogWarning("There should never be more than one Singleton of type '" +
+							typeof(T) + "' in the scene, but " + instances.Length +
+							" were found. The first instance found will be used, and all others destroyed.");
+
+						for (var i = 1; i < instances.Length; i++)
+						{
+							Destroy(instances[i].gameObject);
+						}
+
 						return _instance = instances[0];
 					}
 
-					Debug.LogWarning("There should never be more than one Singleton of type '" +
-						typeof(T) + "' in the scene, but " + instances.Length +
-						" were found. The first instance found will be used, and all others destroyed.");
+					GameObject singleton = new GameObject();
+					_instance = singleton.AddComponent<T>();
+					singleton.name = "_" + typeof(T).ToString() + "(Singleton)";
+					DontDestroyOnLoad(singleton);
 
-					for (var i = 1; i < instances.Length; i++)
-					{
-						Destroy(instances[i].gameObject);
-					}
+					//Debug.Log("[Singleton] An instance of " + typeof(T) +
+					//" is needed in the scene, so '" + singleton + "' was created with DontDestroyOnLoad.");
 
-					return _instance = instances[0];
+					return _instance;
 				}
-
-				GameObject singleton = new GameObject();
-				_instance = singleton.AddComponent<T>();
-				singleton.name = "_" + typeof(T).ToString() + "(Singleton)";
-				DontDestroyOnLoad(singleton);
-
-				//Debug.Log("[Singleton] An instance of " + typeof(T) +
-				//" is needed in the scene, so '" + singleton + "' was created with DontDestroyOnLoad.");
-
-				return _instance;
 			}
 		}
+
+		//protected virtual void Awake()
+		//{
+		//	if (_instance == null)
+		//	{
+		//		_instance = this as T;
+		//	}
+		//	else
+		//	{
+		//		Destroy(gameObject);
+		//	}
+		//	DontDestroyOnLoad(this.gameObject);
+		//}
 	}
 
-	//protected virtual void Awake()
-	//{
-	//	if (_instance == null)
-	//	{
-	//		_instance = this as T;
-	//	}
-	//	else
-	//	{
-	//		Destroy(gameObject);
-	//	}
-	//	DontDestroyOnLoad(this.gameObject);
-	//}
-}
-
-public abstract class Singleton : MonoBehaviour
-{
-	public static bool ApplicationIsQuitting { get; private set; }
-
-	private void OnApplicationQuit()
+	public abstract class Singleton : MonoBehaviour
 	{
-		ApplicationIsQuitting = true;
+		public static bool ApplicationIsQuitting { get; private set; }
+
+		private void OnApplicationQuit()
+		{
+			ApplicationIsQuitting = true;
+		}
 	}
 }
